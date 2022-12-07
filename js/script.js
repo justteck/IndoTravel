175187d3e642214ad5aa2b26c7eb30f2fc18d440
justteck <justteck@yandex.ru>
2022-12-07 17:15:58 +0300

    module_3_lesson_01_extra_02

--- js/script.js
+++ js/script.js
@@ -1,7 +1,13 @@
 import {timer} from './modules/timer.js';
 
+// for plugin
+// import {runTimerPlugin} from './modules/timerPlugin.js';
+
 const init = () => {
   timer();
+
+  // run plugin
+  // runTimerPlugin();
 };
 
 init();
