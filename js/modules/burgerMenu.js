38a317a69519a03d410467e3b230da2f5f092f98
justteck <justteck@yandex.ru>
2022-12-13 18:58:22 +0300

    module_3_lesson_03_fix_1

--- js/modules/burgerMenu.js
+++ js/modules/burgerMenu.js
@@ -4,11 +4,13 @@ const menuBtn = document.querySelector('.header__menu-button');
 const navigation = document.querySelector('.header__menu');
 
 const menuToggle = () => navigation.classList.toggle('header__menu_active');
+
 const menuClose = () => navigation.classList.remove('header__menu_active');
+
 const menuControl = ({target}) => {
   if (target === menuBtn) {
     menuToggle();
-  } else {
+  } else if (navigation.classList.contains('header__menu_active')) {
     menuClose();
   }
 };
